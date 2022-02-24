a921bc5924ff901c657dcdefe2284ce62d5988a4
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-23 18:00:55 -0600

    Creando input en el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,32 @@
 export const Formulario = () => {
   return (
-    <div className="md:w-1/2 lg:w-2/5 mx-5">
+    <div className="md:w-1/2 lg:w-2/5 mx-20">
       <h2 className="font-black text-3xl text-center">
         Seguimiento de pacientes
       </h2>
 
-      <p className="text-lg mt-5 text-center">
+      <p className="text-lg mt-5 text-center mb-10">
         Añadir pacientes y{' '}
         <span className="text-indigo-600 font-bold">administralos</span>
       </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre mascota
+          </label>
+          <input
+            type="text"
+            id="name"
+            htmlFor="name"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
+          />
+        </div>
+      </form>
     </div>
   )
 }
