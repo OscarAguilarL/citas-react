73e0e854b3d67486c247dc7ab5ea01eac9a75e6e
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-23 22:17:38 -0600

    Leyendo el resto de los inputs y agregandolos al state

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 
 export const Formulario = () => {
   const [nombre, setNombre] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintomas, setSintomas] = useState('')
 
-  console.log(nombre)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
 
   return (
-    <div className="md:w-1/2 lg:w-2/5 mx-20">
-      <h2 className="font-black text-3xl text-center">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+      <h2 className="font-black text-2xl text-center">
         Seguimiento de pacientes
       </h2>
 
@@ -16,7 +23,10 @@ export const Formulario = () => {
         <span className="text-indigo-600 font-bold">administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-16">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-16"
+      >
         <div className="mb-5">
           <label
             htmlFor="mascota"
@@ -45,6 +55,8 @@ export const Formulario = () => {
             type="text"
             id="propietario"
             htmlFor="propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
             placeholder="Nombre del dueño"
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           />
@@ -60,6 +72,8 @@ export const Formulario = () => {
             type="email"
             id="email"
             htmlFor="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Correo electronico"
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           />
@@ -75,6 +89,8 @@ export const Formulario = () => {
             type="date"
             id="alta"
             htmlFor="alta"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
             placeholder="Correo electronico"
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           />
@@ -89,6 +105,8 @@ export const Formulario = () => {
           <textarea
             name="sintomas"
             id="sintomas"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
             placeholder="Describe los sintomas"
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           ></textarea>
